3f5da4d42f34f6fffc120d9b16c8b55dde6bf762
Drestanta <118432795+Drestanta@users.noreply.github.com>
2022-12-01 16:57:38 +0700

    Update 2_D_71220895.py

--- 2_D_71220895.py
+++ 2_D_71220895.py
@@ -1,1 +1,11 @@
-pilihan = int(input("Masukkan kata : "))
+def pilihanpilih() :
+    pilih=(input("Masukkan kata :"))
+    list=(pilih)
+    len(pilih)
+    if(len(pilih)%2==0) :
+        print("Huruf paling ujung pada kata", pilih, "adalah", (pilih[-3:]))
+    else :
+        print("Huruf paling ujung pada kata", pilih, "adalah",(pilih[:3]))
+    return (pilih)
+
+pilihanpilih()
