fcfa65888cbcbce62b8b039a9505ddd40453cc4f
Drestanta <118432795+Drestanta@users.noreply.github.com>
2022-12-01 16:06:05 +0700

    Add files via upload

---
+++ 1_D_71220895.py
@@ -0,0 +1,56 @@
+
+print("Halo Selamat datang di bioskop! ")
+print("Dimanakah kamu ingin menonton? ")
+print("1) XXI Empire")
+print("2) XXI Amplaz")
+print("3) XXI JCM")
+pilihan = int(input("Masukkan pilihanmu: "))
+if pilihan == 1 :
+    print("Mau nonton film apa nih? Ada film:")
+elif pilihan == 2 :
+    print("Mau nonton film apa nih? Ada film:")
+elif pilihan == 3 :
+    print("Mau nonton film apa nih? Ada film:")
+elif pilihan == 4 :
+    print("Pilihan tidak tersedia.")
+
+print(" 1. Frozen")
+print(" 2. Keramat")
+print(" 3. KKN di Desa Penari")
+pilihan2 = int(input("Pilihan nomor film: "))
+if pilihan2 == 1 :
+    print("Mau nonton layar bioskop apa:")
+elif pilihan2 == 2 :
+    print("Mau nonton layar bioskop apa:")
+elif pilihan2 == 3 :
+    print("Mau nonton layar bioskop apa:")
+elif pilihan2 == 4 :
+    print("Pilihan tidak tersedia.")
+
+print(" 1. Reguler")
+print(" 2. Dolby almos")
+print(" 3. Premier")
+pilihan3 = int(input("Pilihan nomor tipe layar: "))
+if pilihan3 == 1 :
+    print("Berapa banyak tiket? ")
+elif pilihan3 == 2 :
+    print("Berapa banyak tiket? ")
+elif pilihan3 == 3 :
+    print("Berapa banyak tiket? ")
+elif pilihan3 == 4 :
+    print("Pilihan tidak tersedia.")
+
+print("Mau nonton jam berapa: ")
+print(" 1. 12.35")
+print(" 2. 14.45")
+print(" 3. 16.55")
+print(" 4. 19.05")
+pilihan4 = int(input("Masukkan pilihan anda: "))
+if pilihan4 == 1 :
+    print("Oke berhasil!, silahkan dinikmati di jam 12.35 ")
+elif pilihan4 == 2 :
+    print("Oke berhasil!, silahkan dinikmati di jam 14.45 ")
+elif pilihan4 == 3 :
+    print("Oke berhasil!, silahkan dinikmati di jam 16.55 ")
+elif pilihan4 == 4 :
+    print("Oke berhasil!, silahkan dinikmati di jam 19.05")
